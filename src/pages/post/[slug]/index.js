529c42641bd63808288342f68b122d819b902b2a
Pallas <tyler@tylerhughes.info>
2023-12-31 03:39:55 -0600

    Remove the use of dangerouslySetInnerHTML

--- src/pages/post/[slug]/index.js
+++ src/pages/post/[slug]/index.js
@@ -1,15 +1,15 @@
-// pages/post/[slug]/index.js
 import { useRouter } from 'next/router';
-import { getPostData } from '../../../lib/posts';
+import { getPostData } from '@lib/posts'; // Updated import path
+import Post from '@components/Post'; // Updated import path
 
-export default function BlogPost({ contentHtml }) {
+export default function BlogPostPage({ content }) {
     const router = useRouter();
     const { slug } = router.query;
 
     return (
         <div>
             <h1>{slug}</h1>
-            <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+            <Post content={content} />
         </div>
     );
 }
@@ -19,16 +19,17 @@ export async function getStaticProps({ params }) {
     const postData = await getPostData(`${slug}.md`);
     return {
         props: {
-            ...postData,
+            content: postData.content, // Ensure this is the Markdown content
         },
     };
 }
 
 export async function getStaticPaths() {
-    const postFileNames = fs.readdirSync(contentDirectory);
-    const paths = postFileNames.map((fileName) => ({
-        params: { slug: fileName.replace(/\.md$/, '') },
-    }));
+    // Your logic to generate paths for static generation
+    // Replace with your actual logic to list all markdown files
+    const paths = [
+        // Example: { params: { slug: 'my-first-post' } }
+    ];
     return {
         paths,
         fallback: false,
